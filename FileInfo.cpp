32a9af1998907a6806098894f22c2870f246dd30
zhengsong <741633710@qq.com>
2018-02-28 19:31:39 +0800

    ok

--- FileInfo.cpp
+++ FileInfo.cpp
@@ -3,6 +3,7 @@
 #include "ntfs.h"
 #include <windows.h>
 #include <stdlib.h>
+#include <wchar.h>
 #include <string>
 #include <iostream>
 #include <vector>
@@ -12,7 +13,8 @@ using namespace std;
 ULONG BytesPerFileRecord; // LoadMFT()
 HANDLE hVolume;           // main()
 BOOT_BLOCK bootb;         // main().ReadFile()
-PFILE_RECORD_HEADER MFT; // LoadMFT()
+PFILE_RECORD_HEADER MFT; // LoadMFT()				 
+WCHAR drive[] = L"\\\\.\\C:"; // Default primary partition
 
 // Template for padding
 template <class T1, class T2> inline T1* Padd(T1* p, T2 n)
@@ -95,10 +97,9 @@ PATTRIBUTE FindAttribute(PFILE_RECORD_HEADER file, ATTRIBUTE_TYPE type, PWSTR na
 	{
 		if (attr->AttributeType == type)
 		{
-			if (name == 0 && attr->NameLength == 0)
+			/*if (name == 0 && attr->NameLength == 0)
 				return attr;
-			if (name != 0 && wcslen(name) == attr->NameLength && _wcsicmp(name,
-				PWSTR(Padd(attr, attr->NameOffset))) == 0)
+			if (name != 0 && wcslen(name) == attr->NameLength && _wcsicmp(name, PWSTR(Padd(attr, attr->NameOffset))) == 0)*/
 				return attr;
 		}
 	}
@@ -138,7 +139,7 @@ VOID ReadLCN(ULONGLONG lcn, ULONG count, PVOID buffer)
 }
 
 // 读非常驻属性
-VOID ReadExternalAttribute(PNONRESIDENT_ATTRIBUTE attr, ULONGLONG vcn, ULONG count, string clus, PVOID buffer)
+VOID ReadExternalAttribute(PNONRESIDENT_ATTRIBUTE attr, ULONGLONG vcn, ULONG count, string &clus, PVOID buffer)
 {
 	ULONGLONG lcn, runcount;
 	ULONG readcount, left;
@@ -157,7 +158,9 @@ VOID ReadExternalAttribute(PNONRESIDENT_ATTRIBUTE attr, ULONGLONG vcn, ULONG cou
 		{
 			printf("起始簇:%lld    簇数:%ld\n", lcn, readcount);
 			clus.append(to_string(lcn));  // ULONGLONG 转换为 string
+			clus.append(",");
 			clus.append(to_string(readcount));
+			clus.append(",");
 			// 读连续readcount个簇的数据到内存空间bytes
 			ReadLCN(lcn, readcount, bytes);
 		}
@@ -176,7 +179,7 @@ ULONG AttributeLengthAllocated(PATTRIBUTE attr)
 	return attr->Nonresident == FALSE ? PRESIDENT_ATTRIBUTE(attr)->ValueLength : ULONG(PNONRESIDENT_ATTRIBUTE(attr)->AllocatedSize);
 }
 
-VOID ReadAttribute(PATTRIBUTE attr, string clus, PVOID buffer)
+VOID ReadAttribute(PATTRIBUTE attr, string &clus, PVOID buffer)
 {
 	PRESIDENT_ATTRIBUTE rattr = NULL;
 	PNONRESIDENT_ATTRIBUTE nattr = NULL;
@@ -222,7 +225,7 @@ VOID ReadFileRecord(ULONG index, PFILE_RECORD_HEADER file)
 	// MFT 记录号为 index 的记录，在MFT中的起始VCN
 	ULONGLONG vcn = ULONGLONG(index) * BytesPerFileRecord / bootb.BytesPerSector / bootb.SectorsPerCluster;
 	// 将起始VCN为vcn，连续clusters个簇读到内存空间p中（将index文件记录读到p中）
-	ReadVCN(MFT, AttributeData, vcn, clusters,0,p);
+	ReadVCN(MFT, AttributeData, vcn, clusters,"",p);
 	// m = 每个簇可以包含的文件记录数 - 1
 	LONG m = (bootb.SectorsPerCluster * bootb.BytesPerSector / BytesPerFileRecord) - 1;
 	// 在MFT中，记录号为index的文件记录在该簇中偏移的文件记录数
@@ -255,8 +258,6 @@ BOOL bitset(PUCHAR bitmap, ULONG i)
 
 VOID Initiate(WCHAR *disk)
 {
-	// Default primary partition
-	WCHAR drive[] = L"\\\\.\\C:";
 	ULONG n;
 	// Read the user input
 	drive[4] = *disk;
@@ -287,55 +288,99 @@ VOID Initiate(WCHAR *disk)
 	LoadMFT();
 }
 
-VOID DumpClusNum(ULONG index, string clus, PVOID buf) 
+/*WCHAR* CharToWchar(UCHAR* c)
 {
-	PATTRIBUTE attr = NULL;
-	PFILE_RECORD_HEADER file = PFILE_RECORD_HEADER(new UCHAR[BytesPerFileRecord]);
-	ReadFileRecord(index, file);// 将记录号为index的文件记录读到内存空间file中
-	if (file->Ntfs.Type != 'ELIF')
-		return;
-	attr = FindAttribute(file, AttributeData, 0); // 在内存空间file中找80属性
-	if (attr == 0)
-		return;
-	ReadAttribute(attr, clus, buf); // 将内存空间file中的80属性写到buf中,同时将簇号写到clus
-}
-
-VOID DumpData(ULONGLONG lcn, ULONG readcount, PVOID buffer)
-{
-	ReadLCN(lcn, readcount, buffer);  // 读簇，存入buffer
-}
-
-WCHAR* CharToWchar(WCHAR* m_wchar, LPCSTR c)
-{
-	int len = MultiByteToWideChar(CP_ACP, 0, c, strlen(c), NULL, 0);
-	m_wchar = new WCHAR[len + 1];
-	MultiByteToWideChar(CP_ACP, 0, c, strlen(c), m_wchar, len);
-	m_wchar[len] = '\0';
+	//char* to wchar_t*
+	size_t size = strlen(c) + 1;
+	wchar_t* w_arr_name = new wchar_t[size];
+	mbstowcs(w_arr_name, c, size);
 	return m_wchar;
-}
+}*/
 
 WCHAR* StringToWchar(const string& str)
 {
 	WCHAR* m_wchar = NULL;
 	LPCSTR p = str.c_str();
-	return CharToWchar(m_wchar, p);
+	int len = MultiByteToWideChar(CP_ACP, 0, p, strlen(p), NULL, 0);
+	m_wchar = new WCHAR[len + 1];
+	MultiByteToWideChar(CP_ACP, 0, p, strlen(p), m_wchar, len);
+	m_wchar[len] = '\0';
+	return m_wchar;
+}
+
+// 找到目标索引项后，读MFT记录号，4 个字节表示？
+ULONG ReadIndex(PUCHAR entry)
+{
+	ULONG index = 0;
+
+	for (int i = 3; i >= 0; i--)
+		index = (index << 8) + entry[i];
+
+	return index;
 }
 
 // 根据目录名或文件名，遍历索引项，返回目录或文件的MFT记录号
-ULONG TraverseIndexEntry(ATTRIBUTE_TYPE type, PUCHAR buf, WCHAR *dir)
+ULONG TraverseIndexEntry(ATTRIBUTE_TYPE type, PVOID buf, WCHAR *dir)
 {
+	ULONG index = 0;
 	if (type == AttributeIndexRoot) // 遍历90属性内的索引项，buf中是90属性从第一个索引项开始的属性体，直接遍历索引项
-	{
-
+	{ 
+		PINDEX_ROOT indexRoot = PINDEX_ROOT(buf);;
+		PDIRECTORY_ENTRY entry = PDIRECTORY_ENTRY(Padd(indexRoot, 0x20));  // entry指向了第一个索引项
+		USHORT indexEntryLen = entry->Length;                   // 第一个索引项长度
+		while (indexEntryLen != 0x18 && indexEntryLen != 0x10)  // 若索引项长度为0x18或0x10，结束遍历该簇
+		{
+			PFILENAME_ATTRIBUTE indexEntryFileName = PFILENAME_ATTRIBUTE(Padd(entry, 0x10));  // indexEntryFileName指向索引项中的FILENAME_ATTRIBUTE
+			WCHAR *fileName = new WCHAR[indexEntryFileName->NameLength + 1];     // 第一个索引项的文件名
+			UCHAR nameLen = indexEntryFileName->NameLength;
+			wmemcpy(fileName, PCWSTR(Padd(indexEntryFileName, 0x42)), nameLen); // 读索引项中的文件名
+			fileName[indexEntryFileName->NameLength] = '\0';
+			if (wcscmp(fileName, dir) == 0) // 相等
+			{
+				index = ReadIndex(PUCHAR(entry));
+				return index;
+			}
+			else {
+				entry = PDIRECTORY_ENTRY(Padd(entry, indexEntryLen));  // 指向下一个索引项
+				indexEntryLen = entry->Length;                             // 下一个索引项大小
+			}
+		}
 	}
 	else if (type == AttributeIndexAllocation) { 
 	/* 遍历A0属性指向的索引节点中的索引项，buf中是一个个以INDX开始的索引节点（簇） ，先按索引节点遍历；
 	再在节点内部遍历索引项，当遍历到索引项大小为0x10或0x18时表示该节点的索引项已遍历完，接着遍历下一个节点（簇）*/
-
+		PINDEX_BLOCK_HEADER indexBlock = PINDEX_BLOCK_HEADER(buf);
+		while (indexBlock->Ntfs.Type == 'XDNI')
+		{
+			ULONG firstEntryOffset = indexBlock->DirectoryIndex.EntriesOffset;  // 第一条索引项（DIRECTORY_ENTRY）在该结构内部的字节偏移量
+			PDIRECTORY_ENTRY entry = PDIRECTORY_ENTRY(Padd(indexBlock, 0x18 + firstEntryOffset));  // entry指向了第一个索引项
+			USHORT indexEntryLen = entry->Length;                   // 第一个索引项长度
+			while (indexEntryLen != 0x18 || indexEntryLen != 0x10)  // 若索引项长度为0x18或0x10，结束遍历该簇
+			{
+				PFILENAME_ATTRIBUTE indexEntryFileName = PFILENAME_ATTRIBUTE(Padd(entry, 0x10));  // indexEntryFileName指向索引项中的FILENAME_ATTRIBUTE
+				WCHAR *fileName = new WCHAR[indexEntryFileName->NameLength + 1];     // 第一个索引项的文件名
+				UCHAR nameLen = indexEntryFileName->NameLength;
+				PCWSTR source = PCWSTR(Padd(indexEntryFileName, 0x42));
+				wmemcpy(fileName, source, nameLen); // 读索引项中的文件名
+				fileName[indexEntryFileName->NameLength] = '\0';
+				if (wcscmp(fileName, dir) == 0) // 相等
+				{
+					index = ReadIndex(PUCHAR(entry));
+					delete[]fileName;
+					return index;
+				}
+				else {
+					entry = PDIRECTORY_ENTRY(Padd(entry, indexEntryLen));  // 指向下一个索引项
+					indexEntryLen = entry->Length;                             // 下一个索引项大小
+				}
+				delete[]fileName;
+			}
+			indexBlock = Padd(indexBlock, bootb.SectorsPerCluster * bootb.BytesPerSector);  // 指向下一个索引节点
+		}
 	}
 }
 
-VOID FindMFTRecNum(string filepath)
+ULONG FindMFTRecNum(string filepath)
 {
 	// string -> const char* -> char*
 	LPSTR cpath = new CHAR[filepath.length() + 1];
@@ -353,10 +398,10 @@ VOID FindMFTRecNum(string filepath)
 		token = strtok_s(NULL, seps, &next_token);
 	}
 	WCHAR **dir = new WCHAR *[str_vec.size()];
-	for (int i = 0; i < str_vec.size(); i++)
+	for (unsigned int i = 0; i < str_vec.size(); i++)
 	{
 		dir[i] = StringToWchar(str_vec[i]);
-		wprintf(L"%x\n", dir[i]);
+		//wprintf(L"%x\n", dir[i]);
 	}
 
 	// LoadMFT
@@ -366,38 +411,46 @@ VOID FindMFTRecNum(string filepath)
 	ULONG index = 5;
 	PFILE_RECORD_HEADER file = NULL;
 	PATTRIBUTE attr90 = NULL;
-	for (int i = 1; i < str_vec.size(); i++)
+	for (unsigned int i = 1; i < str_vec.size(); i++)
 	{
 		file = PFILE_RECORD_HEADER(new UCHAR[BytesPerFileRecord]);
 		ReadFileRecord(index, file); // 将MFT记录号为index的文件记录读到内存空间file中
 		if (file->Ntfs.Type != 'ELIF')
-			return;
-		attr90 = FindAttribute(file, AttributeIndexRoot, 0); // 在内存空间file中找90属性
+			return 0;
+		PWSTR name = _wcsdup(L"$I30");
+		attr90 = FindAttribute(file, AttributeIndexRoot, name); // 在内存空间file中找90属性
 		if (attr90 == 0)
-			return;
+			return 0;
 		PUCHAR buffer1 = new UCHAR[AttributeLengthAllocated(attr90)]; // 申请90属性大小的内存空间
-		ReadAttribute(attr90, 0, buffer1); // 将内存空间file中的90属性写到buf1中
-		if (FindAttribute(file, AttributeIndexAllocation, 0) == 0) // 90属性第一种情况，无A0、B0属性
+		string clus = "";
+		ReadAttribute(attr90, clus, buffer1); // 将内存空间file中的90属性写到buf1中
+
+		PRESIDENT_ATTRIBUTE rattr90 = PRESIDENT_ATTRIBUTE(attr90);  // 90属性为常驻属性
+
+		if (FindAttribute(file, AttributeIndexAllocation, 0) == 0) // 90属性第一种情况，无A0、B0属性   
 		{
 			index = TraverseIndexEntry(AttributeIndexRoot, buffer1, dir[i]);
+			delete[]buffer1;
 			continue;
 		}
-		else if (第一个索引项偏移4@0x30 == 0 && 2@3C == 1){ // 90属性第二、四种情况，为大索引，有A0、B0属性
+		else if (rattr90->ValueLength == 0x38){ // 90属性第二、四种情况，为大索引，有A0、B0属性
 			PATTRIBUTE attrA0 = FindAttribute(file, AttributeIndexAllocation, 0); //在内存file中找A0属性 
 			PUCHAR buffer2 = new UCHAR[AttributeLengthAllocated(attrA0)]; // 申请A0属性大小的内存空间
 			/***** 待补充：在ReadAttribute时，需要读B0属性，过滤掉无效的索引节点 */
-			ReadAttribute(attrA0, 0, buffer2); // 将内存空间file中的A0属性写到buf2中，A0是非常驻属性，所以索引节点的数据全被写入buf2
+			ReadAttribute(attrA0, clus, buffer2); // 将内存空间file中的A0属性写到buf2中，A0是非常驻属性，所以索引节点的数据全被写入buf2
 			index = TraverseIndexEntry(AttributeIndexAllocation, buffer2, dir[i]); // 遍历buf2中索引节点的索引项，找到下一个目录的MFT记录号
+			delete[]buffer2;
 			continue;
 		}
-		else if (偏移4@0x30 == 0x10 && 2@3C == 1){ // 90属性第三种情况
+		else if (rattr90->ValueLength > 0x38){ // 90属性第三种情况
 			ULONG indexTemp = TraverseIndexEntry(AttributeIndexRoot, buffer1, dir[i]); // 分别遍历90内的索引项和A0指向的索引节点，找目标文件名
 			if (indexTemp == 0) 
 			{
 				PATTRIBUTE attrA0 = FindAttribute(file, AttributeIndexAllocation, 0);//在内存file中找A0属性
 				PUCHAR buffer2 = new UCHAR[AttributeLengthAllocated(attrA0)]; // 申请A0属性大小的内存空间
-				ReadAttribute(attrA0, 0, buffer2);
+				ReadAttribute(attrA0, clus, buffer2);
 				index = TraverseIndexEntry(AttributeIndexAllocation, buffer2, dir[i]);
+				delete[]buffer2;
 				continue;
 			}
 			else {
@@ -408,10 +461,78 @@ VOID FindMFTRecNum(string filepath)
 	}
 
 	// delete **dir
-	for (int i = 0; i<str_vec.size(); i++)
+	for (unsigned int i = 0; i<str_vec.size(); i++)
 	{
 		delete[] dir[i];
 	}
 	delete dir;
-	delete[] cpath;
+
+	//delete[] cpath;  // 释放内存出错
+
+	return index;
+}
+
+// 主要功能函数1，输入路径、簇字符串、内存buf、buf的大小
+UCHAR* DumpClusNum(string path, string &clus, ULONG *attributeLengthAllocated)
+{
+	ULONG index = FindMFTRecNum(path);
+	PATTRIBUTE attr = NULL;
+	PFILE_RECORD_HEADER file = PFILE_RECORD_HEADER(new UCHAR[BytesPerFileRecord]);
+	ReadFileRecord(index, file);// 将记录号为index的文件记录读到内存空间file中
+	if (file->Ntfs.Type != 'ELIF')
+		return NULL;
+	attr = FindAttribute(file, AttributeData, 0); // 在内存空间file中找80属性
+	if (attr == 0)
+		return NULL;
+	*attributeLengthAllocated = AttributeLengthAllocated(attr);
+	UCHAR *buf = new UCHAR[*attributeLengthAllocated];
+	wprintf(L"dump clus and data starts...\n");
+	ReadAttribute(attr, clus, buf); // 将内存空间file中的80属性写到buf中,同时将簇号写到clus
+	return buf;
+}
+
+// 主要功能函数2，输入逻辑簇号、簇数、内存buf
+VOID DumpData(WCHAR *disk, ULONGLONG lcn, ULONG readcount, PVOID buffer)
+{
+
+	// LoadMFT
+	Initiate(disk);
+	wprintf(L"dump data starts...\n");
+	ReadLCN(lcn, readcount, buffer);  // 读簇，存入buffer
+}
+
+int wmain(int argc, WCHAR **argv)
+{
+	// 测试：根据MFT记录号获取文件的簇和数据
+	string path = "F:\\JavaWeb\\new.txt";
+	PVOID bufFileData = NULL; //定义为全局变量?
+	string clus = "";
+	ULONG n;
+	ULONG *fileLengthAllocated = new ULONG;
+	bufFileData = DumpClusNum(path,clus, fileLengthAllocated);
+	//wprintf(L"%.*s\n", clus);
+
+	// 创建文件filename
+	LPCWSTR filename = L"f:\\new.txt";
+	HANDLE hFile = CreateFile(filename, GENERIC_WRITE, 0, 0, CREATE_ALWAYS, 0, 0);
+	if (hFile == INVALID_HANDLE_VALUE)
+	{
+		wprintf(L"CreateFile() failed, error %u\n", GetLastError());
+		return 0;
+	}
+	// 将bufFileData（文件的80属性）写到filename文件中
+	if (WriteFile(hFile, bufFileData, *fileLengthAllocated, &n, 0) == 0)
+	{
+		wprintf(L"WriteFile() failed, error %u\n", GetLastError());
+		return 0;
+	}
+
+	CloseHandle(hFile);
+	delete[] bufFileData;
+
+	// 测试：输入起始簇和簇数，输出簇内容
+
+
+	getchar();
+	return 0;
 }
